403cef9505748543beaca967fc9e6fe8686f14bf
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-09-11 14:15:17 -0300

    commit

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -14,9 +14,9 @@ export class NegociacaoController {
     }
 
     adiciona(): void {
-        const negociacao = this.criaNegociacao();
+        const negociacao = this.criaNegociacao();        
+        negociacao.data.setDate(12);
         this.negociacoes.adiciona(negociacao);
-        this.negociacoes.lista();
         console.log(this.negociacoes.lista());
         this.limpaFormulario();
     }
